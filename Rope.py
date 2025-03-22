8872e0f9e5a951cd42cfefd0f299c60f05dff586
kirkmcguire <kirkothername@gmail.com>
2025-03-22 01:09:18 -0700

    commit

--- Rope.py
+++ Rope.py
@@ -3,3 +3,11 @@
 from rope import Coordinator
 if __name__ == "__main__":
     Coordinator.run()
+    python rope.py cd c:
+    C:\Users\kirk\Desktop\rope\Rope
+    cd C:\Users\kirk\Desktop\rope\Rope
+    rope.py
+    cd C:\Users\kirk\Desktop\rope\Rope
+    python rope.py
+    
+    
